a5929a581f1d522729969fc03470dc7dd1c62fc1
zblack6 <155221176+zblack6@users.noreply.github.com>
2024-07-29 21:31:21 +0800

    scss自动导入

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,16 @@ import { RouterView } from 'vue-router'
   </header>
 
   <RouterView />
+  <div class="test">
+    test scss
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test{
+  color: $priceColor;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
