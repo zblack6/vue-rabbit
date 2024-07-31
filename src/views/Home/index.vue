7da4ab477e644e9f8517cb6599cdd61ae00bd14c
zblack6 <155221176+zblack6@users.noreply.github.com>
2024-07-31 21:18:24 +0800

    分类和轮播图

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,18 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue';
+import HomeBanner from './components/HomeBanner.vue';
+import HomeHot from './components/HomeHot.vue';
+import HomeNew from './components/HomeNew.vue';
+import HomeProduct from './components/HomeProduct.vue';
+
+</script>
+
 <template>
-    <div style="height: 1200px;">Home</div>
+    <div class="container">
+        <HomeCategory />
+        <HomeBanner />
+        </div>
+    <HomeNew />
+    <HomeHot />
+    <HomeProduct />
 </template>
