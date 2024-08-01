62b8e8e197ab0d0f38f6ffaece2037a32cedaaf8
zblack6 <155221176+zblack6@users.noreply.github.com>
2024-08-01 14:11:09 +0800

    banner轮播图渲染

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,5 +1,18 @@
 
 <script setup>
+  import {getBannerAPI} from '@/apis/home'
+import {ref,onMounted} from 'vue'
+
+const bannerList = ref([])
+
+const getBanner = async() => {
+  const res = await getBannerAPI()
+  console.log(res)
+  bannerList.value = res.result
+} 
+
+onMounted(()=>getBanner())
+
 
 </script>
 
@@ -8,8 +21,8 @@
 <template>
   <div class="home-banner">
     <el-carousel height="500px">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <img src="http://yjy-xiaotuxian-dev.oss-cn-beijing.aliyuncs.com/picture/2021-04-15/6d202d8e-bb47-4f92-9523-f32ab65754f4.jpg" alt="">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
       </el-carousel-item>
     </el-carousel>
   </div>
