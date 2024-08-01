65f1a38d874c35f42db8ab87bbe1d088d989e18c
zblack6 <155221176+zblack6@users.noreply.github.com>
2024-08-01 14:26:18 +0800

    构建静态模版和插槽props

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,7 @@ import HomeBanner from './components/HomeBanner.vue';
 import HomeHot from './components/HomeHot.vue';
 import HomeNew from './components/HomeNew.vue';
 import HomeProduct from './components/HomeProduct.vue';
-
+import HomePanel from './components/HomePanel.vue';
 </script>
 
 <template>
@@ -15,4 +15,18 @@ import HomeProduct from './components/HomeProduct.vue';
     <HomeNew />
     <HomeHot />
     <HomeProduct />
+
+    <!--测试面板组件-->
+    <HomePanel title="新鲜好物" sub-title="新鲜好物，热气">
+        <div>
+            插槽1
+        </div>
+        
+    </HomePanel>
+    <HomePanel title="人气推荐" sub-title="人气2">
+        <div>
+            插槽2
+        </div>
+    </HomePanel>
+
 </template>
