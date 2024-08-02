754c7d487e5477e5a4ddf7eaba9eee1ec8640e57
zblack6 <155221176+zblack6@users.noreply.github.com>
2024-08-02 21:52:51 +0800

    图片懒载入，只在图片出现在视口的时候进行载入

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 // //测试接口
 // import { getCategory } from './apis/testAPI'
 // getCategory().then(res => {
@@ -20,3 +22,23 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
+//定义全局指令
+app.directive('img-lazy',{
+    mounted(el,binding){
+        //el:指令绑定的那个元素 img
+        //binding： binding.value  指令等于号后面绑定的表达式的值  图片url
+        console.log(el,binding)
+        useIntersectionObserver(
+            el,
+            ([{isIntersecting}])=>{
+                console.log(isIntersecting)
+                if (isIntersecting){
+                    //进入视口区域
+                    el.src = binding.value
+                }
+            }
+        )
+    }
+})
